39240df72f0d08b87ab1b65289193a6366dda730
Vipul Naik <vipulnaik1@gmail.com>
2020-08-03 16:51:04 -0700

    Try adding a summaries by week section

--- access-portal/index.php
+++ access-portal/index.php
@@ -14,9 +14,11 @@ print '<h3>Updates for Vipul Naik</h3>';
 include_once('preamble.inc');
 print '<h4>Table of contents</h4>';
 print '<ul>';
+print '<li><a href="#summariesByWeek">Summaries by week</a></li>';
 print '<li><a href="#summariesByDay">Summaries by day</a></li>';
 print '</ul>';
 
+include ("backend/summariesByWeek.inc");
 include ("backend/summariesByDay.inc");
 
 include_once('anchorjs.inc');
