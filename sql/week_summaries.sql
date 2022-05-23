e4aa682222f4f5cd1d5a0d2af9fc7b81074222f3
Vipul Naik <vipulnaik1@gmail.com>
2022-05-23 08:19:32 -0700

    Add week summary for week of 2022-05-16 to 2022-05-22

--- sql/week_summaries.sql
+++ sql/week_summaries.sql
@@ -83,4 +83,5 @@ insert into week_summaries(week_start_date, notes) values
   ('2022-04-18','This was the week that I moved house, so I spent a lot of time on the move and on related disposal and cleanup work. It was also the week that I started part-time at Equator (on 2022-04-19) so starting this week, my time on LiftIgniter / Arena Group work fell and my time on Equator rose.'),
   ('2022-04-25','I continued to spend a fair amount of time on cleanup and disposal related to the house I had moved out of; this ate into my time in the various other buckets.'),
   ('2022-05-02','I spent even more time than in the past two weeks on cleanup and disposal related to the house I had moved out of; this ate into my time in the various other buckets. I finished up most of the work needed so I expect the coming week and future weeks to be closer to normal.'),
-  ('2022-05-09','This was a typical week, hitting the desired distribution of time spent that I hope to maintain for the next few months.');
+  ('2022-05-09','This was a typical week, hitting the desired distribution of time spent that I hope to maintain for the next few months.'),
+  ('2022-05-16','This was a somewhat atypical week, with slightly less time on Equator than I would ideally like to spend, but more time on personal projects, most of it spent on server devops streamlining.');
