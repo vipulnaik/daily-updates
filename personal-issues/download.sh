e1e3182f11d2a20f8cc30fc692332ac38345fa35
Vipul Naik <vipulnaik1@gmail.com>
2022-07-04 15:05:06 -0700

    Add personal issues for week of 2022-06-27 to 2022-07-03

--- personal-issues/download.sh
+++ personal-issues/download.sh
@@ -1,4 +1,4 @@
-for i in {1..914} # Change range to cover whatever additional issues need downloading
+for i in {1..920} # Change range to cover whatever additional issues need downloading
 do
         gh issue view $i > $i.md
         echo "downloaded issue $i"
