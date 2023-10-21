88b8244673675c9302339a328e385d0cb174dcf8
Vipul Naik <vipulnaik1@gmail.com>
2023-10-21 09:22:55 -0700

    Add summaries for 2023-10-19 and 2023-10-20

--- personal-issues/download.sh
+++ personal-issues/download.sh
@@ -1,4 +1,4 @@
-for i in {1..1356} # Change range to cover whatever additional issues need downloading
+for i in {1..1358} # Change range to cover whatever additional issues need downloading
 do
         gh issue view $i > $i.md
         echo "downloaded issue $i"
