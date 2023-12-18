68f38ae8df6d591889c9488c4c935b6dc6fa12f7
Vipul Naik <vipulnaik1@gmail.com>
2023-12-18 09:49:09 -0800

    Add summaries for weekend of 2023-12-16 and 2023-12-17; add week summary; fetch latest personal issues

--- personal-issues/download.sh
+++ personal-issues/download.sh
@@ -1,4 +1,4 @@
-for i in {1..1413} # Change range to cover whatever additional issues need downloading
+for i in {1..1415} # Change range to cover whatever additional issues need downloading
 do
         gh issue view $i > $i.md
         echo "downloaded issue $i"
