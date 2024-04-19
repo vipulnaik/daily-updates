8f3b2f53326dc9719f4114cff0f00683d34e5130
Vipul Naik <vipulnaik1@gmail.com>
2024-04-19 11:35:10 -0700

    Add summaries for period of 2024-04-15 to 2024-04-18; fetch latest personal issues

--- personal-issues/download.sh
+++ personal-issues/download.sh
@@ -1,4 +1,4 @@
-for i in {1..1539} # Change range to cover whatever additional issues need downloading
+for i in {1..1544} # Change range to cover whatever additional issues need downloading
 do
         gh issue view $i > $i.md
         echo "downloaded issue $i"
