e2b5348ecbccffb38d60bf6e50bf02dfd15e7a38
Vipul Naik <vipulnaik1@gmail.com>
2024-05-17 14:08:11 -0700

    Attempt a new, improved dark mode by pulling in the latest change-theme.js from @riceissa

--- access-portal/index.php
+++ access-portal/index.php
@@ -13,7 +13,15 @@ include_once('backend/globalVariables/passwordFile.inc');
 print "<body>\n";
 print '<script>$(document).ready(function(){$("table").tablesorter({textExtraction: stripCommas});});</script>'."\n";
 print '<h3>Updates for Vipul Naik</h3>';
-print '<p><span id="changeThemeMenu" style="display: none;">Change <a href="javascript:;" onclick="change_theme_color()">color</a></span>   &thinsp;<!-- blank space to prevent cumulative layout shift --></p>';
+?>
+
+<p><span id="changeThemeMenu" style="display: none;">Set color scheme to:
+         <span id="auto-menu-option" style="cursor: pointer;" class="unselectable" onclick="change_theme.set_color('auto')">auto</span>,
+         <span id="light-menu-option" style="cursor: pointer;" class="unselectable" onclick="change_theme.set_color('light')">light</span>,
+         <span id="dark-menu-option" style="cursor: pointer;" class="unselectable" onclick="change_theme.set_color('dark')">dark</span>
+</span>&thinsp;<!-- blank space to prevent cumulative layout shift --></p>
+
+<?php
 include_once('preamble.inc');
 print '<h4>Table of contents</h4>';
 print '<ul>';
@@ -29,5 +37,6 @@ include ("backend/summariesByWeek.inc");
 include ("backend/summariesByDay.inc");
 
 include_once('anchorjs.inc');
+print '<script>change_theme.set_theme_from_local_storage();</script>';
 print '</body>';
 ?>
