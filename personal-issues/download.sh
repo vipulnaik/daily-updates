1727303a947349c371498593a020a432d6ec9099
Vipul Naik <vipulnaik1@gmail.com>
2024-08-24 12:34:44 -0700

    Tweak time estimate for potatoes prep; fetch latest personal issues

--- personal-issues/download.sh
+++ personal-issues/download.sh
@@ -1,4 +1,4 @@
-for i in {1..1676} # Change range to cover whatever additional issues need downloading
+for i in {1..1677} # Change range to cover whatever additional issues need downloading
 do
         gh issue view $i > $i.md
         echo "downloaded issue $i"
