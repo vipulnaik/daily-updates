8f023b116e4d3c6b972207eb4250c7accc923d9c
Vipul Naik <vipulnaik1@gmail.com>
2025-06-01 12:04:52 +0530

    Add summaries for period of 2025-05-26 to 2025-05-31; add May 2025 month summary; fetch latest personal issues

--- personal-issues/download.sh
+++ personal-issues/download.sh
@@ -1,4 +1,4 @@
-for i in {1..1953} # Change range to cover whatever additional issues need downloading
+for i in {1..1959} # Change range to cover whatever additional issues need downloading
 do
         gh issue view $i > $i.md
         echo "downloaded issue $i"
