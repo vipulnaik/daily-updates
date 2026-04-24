3b6b0022147bd3542423283a8f88c8a7b5fbe756
Vipul Naik <vipulnaik1@gmail.com>
2026-04-23 18:46:56 -0700

    Add reminder in extract_wrapper.sh output to copy; keep it sleeping while it awaits a response

--- extract_wrapper.sh
+++ extract_wrapper.sh
@@ -1,3 +1,4 @@
 # Folder for backup of work daily update is specific to Vipul Naik's machine
 python3 python/extract.py $ADU_ROOT/backup/$1.md personal-issues/$2.md
-
+echo "Copy this over to daily updates row if applicable and hit ^c when done"
+sleep 6000
