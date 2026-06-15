4932191fe1ca47b8f51ac8e6cd46ee75e7d247f3
Vipul Naik <vipulnaik1@gmail.com>
2026-06-14 20:48:09 -0700

    Add verification query confirming presence of notes when the number of cyclic personal chores with long time block commitment is unusual

--- python/day_summaries_verification_queries.py
+++ python/day_summaries_verification_queries.py
@@ -6,6 +6,8 @@ queries = [
     "select * from day_summaries where date < '2018-08-10';",
 
     "select * from day_summaries where minutes_spent_on_day_job_equator is not null and (date < '2022-02-14' or date > '2023-05-28');",
+
+    "select date from day_summaries where (num_cpc_ltbcs = 0 or num_cpc_ltbcs >= 3) and date >= '2025-06-07' and notes is null;",
 ]
 
 _connection = connection.connect()
