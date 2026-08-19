888b0add3712da7605c9d43dfaf7562e0d584a19
Vipul Naik <vipulnaik1@gmail.com>
2026-08-19 13:14:48 -0700

    Add 3-second grace period to download.sh to allow quick cancel

--- personal-issues/download.sh
+++ personal-issues/download.sh
@@ -1,4 +1,9 @@
-for i in $(seq ${1:-1} ${2:-2403}) # Change range to cover whatever additional issues need downloading
+start=${1:-1}
+end=${2:-2403}
+grace=3
+echo "downloading issues $start to $end after $grace seconds"
+sleep $grace
+for i in $(seq $start $end)
 do
         gh issue view $i > $i.md
         echo "downloaded issue $i"
