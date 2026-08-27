faec3b00344ac40b686283cb59fa05b66bb8c174
Vipul Naik <vipulnaik1@gmail.com>
2026-08-26 23:38:27 -0700

    Add summaries for period of 2026-08-21 to 2026-08-23; add corresponding week summary; fetch latest personal issues

--- sql/day_summaries_2024-12-30-to-present.sql
+++ sql/day_summaries_2024-12-30-to-present.sql
@@ -684,4 +684,7 @@ insert into day_summaries(date, day_job_liftigniter_and_arena_daily_update, pers
   ('2026-08-17',6525,2401,178,133,0,0,0,0,50,0,785,'A combination of several day job items and several steps on the ARK conjecture work led me to get sufficiently delayed as to not be able to fit in any cyclic personal chore with long time block commitment (I had been thinking of doing laundry).'),
   ('2026-08-18',6526,2402,41,220,0,60,0,0,24,1,726,null),
   ('2026-08-19',6527,2403,44,108,0,119,0,0,17,1,818,null),
-  ('2026-08-20',6528,2404,196,43,0,1,0,0,143,0,813,'A few lengthy day job investigations through the day, as well as wrapping up of several longstanding day job items, led me to a time squeeze and I ended up not getting enough time to fit in my planned cyclic personal chore with long time block commitment (a Trader Joe''s trip).');
+  ('2026-08-20',6528,2404,196,43,0,1,0,0,143,0,813,'A few lengthy day job investigations through the day, as well as wrapping up of several longstanding day job items, led me to a time squeeze and I ended up not getting enough time to fit in my planned cyclic personal chore with long time block commitment (a Trader Joe''s trip).'),
+  ('2026-08-21',6529,2405,47,59,0,61,0,0,175,1,606,null),
+  ('2026-08-22',null,2406,0,42,60,160,0,0,100,1,562,null),
+  ('2026-08-23',6530,2407,9,30,112,34,0,0,107,1,692,null);
